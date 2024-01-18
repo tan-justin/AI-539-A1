9056c1a434800050791dc8db49d576b5999be3db
tan-justin <tanju@oregonstate.edu>
2024-01-17 22:02:16 -0800

    Update train_eval.py
    Added method A and started on method B

--- train_eval.py
+++ train_eval.py
@@ -19,6 +19,7 @@ class TrainModel:
         self.y_test = None
         self.x_missing = None
         self.y_missing = None
+        self.missing = None
         self.accuracy_dict_entire_test_set = {}
         self.accuracy_dict_missing_values = {}
 
@@ -27,12 +28,13 @@ class TrainModel:
         data = self.data
         Xy = data.to_numpy()
         X = Xy[:,1:]
-        y = (Xy[:,0] >= self.threshold).astype(int)
+        y = (Xy[:,0] >= self.threshold).astype(int) #sets to 0 if star and 1 if galaxy
         missing = np.sum(np.isnan(X), axis = 1) > 0
         self.x_train, self.x_test, self.y_train, self.y_test = \
             train_test_split(X[~missing], y[~missing], train_size = 3000, random_state = self.random_state)
         self.x_missing = X[missing]
         self.y_missing = y[missing]
+        self.missing = missing
 
     def train_model(self):
 
@@ -44,17 +46,37 @@ class TrainModel:
     def evaluate_model(self):
         
         method = ['A','B','C','D','E']
+
         for method in method:
 
             x_test = self.x_test.copy()
             y_test = self.y_test.copy()
             x_missing = self.x_missing.copy()
-            y_missiing = self.y_missing.copy()
+            y_missing = self.y_missing.copy()
             
-            if method == 'A':
+            if method == 'A': #method: Abstention
+
+                x_test_full = np.concatenate(x_test, x_missing)
+                y_pred = self.model.predict(x_test_full)
+                missing_indices = np.where(self.missing)[0]
+
+                y_pred[missing_indices] = -1 
+                #we know that it will never be classified as -1, hence we can trigger a wrong prediction for items with missing values
+
+                accuracy = accuracy_score(y_test, y_pred)
                 
+                self.accuracy_dict_entire_test_set[method] = accuracy
+
+                #because items with missing values are treated as errors, we can ignore it and give it a 0 accuracy
+                self.accuracy_dict_missing_values[method] = 0.0 
+            
+            if method == 'B':
+
+                majority_class = np.bincount(self.y_train).argmax()
+                y_pred_majority = np.full_like(y_missing, fill_value=majority_class)
                 y_pred = self.model.predict(x_test)
-                accuracy = accuracy_score(y_test,y_pred)
+                y_pred[self.missing] = y_pred_majority
+                accuracy = accuracy_score(y_test, y_pred)
                 self.accuracy_dict_entire_test_set[method] = accuracy
 
                 
